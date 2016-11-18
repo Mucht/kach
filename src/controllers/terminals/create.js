99e6540d47496000e1ed8246c82beae8a1cc2363
Mathieu Claessens <mathieu_claessens@hotmail.com>
2016-11-18 12:15:58 +0100

    Export checkBank function

--- src/controllers/terminals/create.js
+++ src/controllers/terminals/create.js
@@ -6,11 +6,10 @@
  * started at 18/11/2016
 */
 
-import Promise from "bluebird";
 import { ObjectID } from "mongodb";
 
 import getTerminals from "../../models/terminals";
-import getBanks from "../../models/banks";
+import { checkBank } from "../../models/banks";
 import { send, error } from "../../core/utils/api";
 import checkPosition from "../../core/utils/position";
 
@@ -25,7 +24,6 @@ export default function( oRequest, oResponse ) {
         sAddress = ( POST.address || "" ).trim(),
         oPosition = checkPosition( iLatitude, iLongitude ),
         oTerminal,
-        fCheckBank,
         fCreateTerminal;
 
     // 1er chose on vérifie sur latitude et longitude sont correct.
@@ -45,37 +43,6 @@ export default function( oRequest, oResponse ) {
 
     // Si on à l'adresse, on l'ajoute dans le tableau oTerminal.
     sAddress && ( oTerminal.adress = sAddress );
-    // Si la bank existe déjà, on l'ajoute PAS dans le tableau oTerminal.
-    // On va le faire en deux fois. Problème : pour vérifier si elle existe on doit vérifier dans la DB et ça prend du temps.
-    // On utilise les promises donc on ajoute dans les import.
-    fCheckBank = () => {
-        let oBankID;
-
-        // si la bank n'existe pas déjà on renvoit false.
-        if ( !sBankID ) {
-            return Promise.resolve( false );
-        }
-
-        // le try permet de ne pas faire planter le tout, mais il passe à catch à la place.
-        // Si ça fonctionne on à un objet oBank.
-        try {
-            oBankID = new ObjectID( sBankID );
-        } catch ( oError ) {
-            return Promise.reject( new Error( "Invalid Bank ID !" ) );
-        }
-
-        return getBanks()
-            .findOne( {
-                "_id": oBankID,
-            } )
-            .then( ( oBank ) => {
-                if ( oBank ) {
-                    return Promise.resolve( true );
-                }
-
-                return Promise.reject( new Error( "Unknown Bank !" ) );
-            } );
-    };
 
     fCreateTerminal = ( bHasBank ) => {
         if ( bHasBank ) {
@@ -87,7 +54,7 @@ export default function( oRequest, oResponse ) {
             .insertOne( oTerminal );
     };
 
-    fCheckBank()
+    checkBank( sBankID )
         .then( fCreateTerminal )
         .then( () => {
             // if all is ok.
--- src/models/banks.js
+++ src/models/banks.js
@@ -6,8 +6,45 @@
  * started at 21/10/2016
 */
 
+import Promise from "bluebird";
 import { db } from "../core/mongodb";
+import { ObjectID } from "mongodb";
 
 export default function() {
     return db.collection( "banks" );
 }
+
+let fCheckBank;
+
+fCheckBank = function( sBankID ) {
+    let oBankID;
+
+    // si la bank n'existe pas déjà on renvoit false.
+    if ( !sBankID ) {
+        return Promise.resolve( false );
+    }
+
+    // le try permet de ne pas faire planter le tout, mais il passe à catch à la place.
+    // Si ça fonctionne on à un objet oBank.
+    try {
+        oBankID = new ObjectID( sBankID );
+    } catch ( oError ) {
+        return Promise.reject( new Error( "Invalid Bank ID !" ) );
+    }
+
+    return db.collection( "banks" )
+        .findOne( {
+            "_id": oBankID,
+        } )
+        .then( ( oBank ) => {
+            if ( oBank ) {
+                return Promise.resolve( true );
+            }
+
+            return Promise.reject( new Error( "Unknown Bank !" ) );
+        } );
+};
+
+export {
+    fCheckBank as checkBank,
+};
