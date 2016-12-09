05ec1f039ef15f52b008bc940ced11aecc25b5a5
Mathieu Claessens <mathieu_claessens@hotmail.com>
2016-12-09 12:23:02 +0100

    Install dependencies & add base dummy modules

---
+++ static/modules/main.js
@@ -0,0 +1,13 @@
+/* ria/kach
+ *
+ * /static/modules/main.js - Main script
+ *
+ * Coded by Mucht - Mathieu Claessens
+ * started at 09/12/2016
+*/
+
+import utils from "./utils.js";
+
+console.log( "Hey from main !" );
+
+utils();
